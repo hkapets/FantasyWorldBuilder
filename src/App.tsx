a9ddeb810aaeb7039ef14c9560215a36ce9796e2
hkapets <hkapets1995@gmail.com>
2025-06-05 20:16:15 +0300

    Add App.tsx to fix import error

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>FantasyWorldBuilder is Working!</h1>
+      <p>This is a test message.</p>
+    </div>
+  );
+};
+
+export default App;
